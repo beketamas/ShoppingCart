d57e5223e9dca4ffe3dd370c4ab852e72a63db7e
beke.tamas <beke.tamas@BURG-1E05-11>
2025-01-24 10:43:57 +0100

    kesz?

--- ShoppingCart/Models/ShoppingCart.cs
+++ ShoppingCart/Models/ShoppingCart.cs
@@ -11,7 +11,16 @@
 
         public int ProductCount => _products.Count;
 
+        public List<Product> Products => _products;
+
+
         //TODO Készítse el a ShoppingCart osztályban azokat a metódusokat, amelyekkel sikeresen lefutnak a tesztesetek!
 
+
+        public void AddProduct(string name, double price) => Products.Add(new Product(name, price));
+        public void RemoveProduct(string name) => Products.Remove(Products.Where(x => x.Name == name).First());
+        public double GetTotalPrice() => Products.Sum(x => x.Price);
+        public List<Product> GetProducts() => _products.Select(x => x).ToList();
+        
     }
 }
